c67f654b1ff8742dd54ec3bec0a66106544ed553
Szabo Gergo <szabo.gergo.bme@gmail.com>
2023-06-28 14:08:37 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Hi, this is the famous band name generator!")
+city_name = input("What city did you grow up in?\n")
+pet_name = input("What is the name of your pet?\n")
+band_name = city_name + " " + pet_name
+print("Your band name could be: " + band_name)
